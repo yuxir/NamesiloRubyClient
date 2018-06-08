407dee6ec60cd5e5b936af531cabf5c826786525
yuxi <me@yuxifan.com>
2018-06-08 23:57:47 +0200

    get rspec for get_account working

--- spec/namesilo_client_spec.rb
+++ spec/namesilo_client_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'namesilo_client'
 require 'dotenv'
+require 'nokogiri'
 
 describe NamesiloClient do
 
@@ -9,8 +10,10 @@ describe NamesiloClient do
 
   it "has get_account" do
     response = client.get_account()
-    puts response
     expect(response.length).to be > 0
+    xml_doc  = Nokogiri::XML(response)
+    expect(xml_doc.xpath('/namesilo/request/operation/text()').to_s).to eq('contactList')
+    expect(xml_doc.xpath('/namesilo/reply/detail/text()').to_s).to eq('success')
   end
 
 end
