e8e344e4c8c7af980c9191fb1d108bfa550c7cb5
yuxi <me@yuxifan.com>
2018-06-08 21:58:28 +0200

    add comments

--- lib/namesilo_client.rb
+++ lib/namesilo_client.rb
@@ -4,12 +4,14 @@ require 'addressable'
 
 module NamesiloClient
   class API
-
+    
+    # Class constructor
     def initialize(apikey)
       @apikey = apikey
       @host   = 'https://www.namesilo.com/api/'
     end
 
+    # Establish connection
     def get_connection()
       conn = Faraday.new(:url => @host) do |c|
         c.use Faraday::Request::UrlEncoded 
@@ -17,6 +19,7 @@ module NamesiloClient
       end
     end
 
+    # Default parameters for Namesilio REST APIs
     def get_default_parameters()
       {"version":"1","type":"xml","key":@apikey}
     end
@@ -25,12 +28,14 @@ module NamesiloClient
       get_connection().get endpoint
     end
 
+    # Construct URL parameters, combing with default parameters
     def get_url_parameters(params)
       uri = Addressable::URI.new
       uri.query_values = params.merge(get_default_parameters())
       uri.query
     end
 
+    # Return namesilo account information
     def get_account()
       get_request('contactList?'+get_url_parameters({})).body
     end
